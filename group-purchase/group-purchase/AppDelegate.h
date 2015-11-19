eebeb4308d9a5a641c06ddb4f3f29975d6b587d9
LeeMinglu <402007353@qq.com>
2015-11-20 06:37:43 +0800

    初始化环境

---
+++ group-purchase/group-purchase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  group-purchase
+//
+//  Created by 李明禄 on 15/11/20.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ group-purchase/group-purchase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  group-purchase
+//
+//  Created by 李明禄 on 15/11/20.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
