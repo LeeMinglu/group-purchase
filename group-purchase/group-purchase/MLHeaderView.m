3c93cccfdd23bce0021b164f787bba9c33eff964
LeeMinglu <402007353@qq.com>
2015-11-21 18:32:55 +0800

    实现图像与pageControl的关联

--- group-purchase/group-purchase/MLHeaderView.m
+++ group-purchase/group-purchase/MLHeaderView.m
@@ -7,11 +7,15 @@
 //
 
 #import "MLHeaderView.h"
+#define count 5
 
-@interface MLHeaderView ()
+
+@interface MLHeaderView ()<UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (nonatomic, strong) NSTimer *timer;
 @end
 
 @implementation MLHeaderView
@@ -26,20 +30,30 @@
     CGFloat width = scFrame.size.width;
     CGFloat height = scFrame.size.height;
     
+    self.scrollView.delegate = self;
     
-    NSUInteger imgCount = 5;
+    NSUInteger imgCount = count;
     
     self.scrollView.contentSize = CGSizeMake(imgCount * width, height);
     
     
-    //设置属性
+    //设置scroll属性
     self.scrollView.pagingEnabled = YES;
     self.scrollView.showsHorizontalScrollIndicator = NO;
     self.scrollView.bounces = NO;
     
+    //设置pageControl属性
+    self.pageControl.numberOfPages = imgCount;
+    self.pageControl.pageIndicatorTintColor = [UIColor darkGrayColor];
+    self.pageControl.currentPageIndicatorTintColor = [UIColor redColor];
+    
     for (int i = 0; i < imgCount; i++) {
         UIImageView *imgView = [[UIImageView alloc] init];
         imgView.backgroundColor = [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
+        
+        NSString *imgName = [NSString stringWithFormat:@"ad_%02d",i];
+        
+        imgView.image = [UIImage imageNamed:imgName];
         
         CGFloat  x =0;
         x += i * width;
@@ -50,7 +64,39 @@
         
         [self.scrollView addSubview:imgView];
     }
+    
+//    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.5 target:self selector:@selector(nextImage) userInfo:nil repeats:YES];
 
+}
+
+- (void)nextImage {
+    
+    CGFloat width = self.scrollView.bounds.size.width;
+    NSUInteger page = 0;
+    
+    if (self.pageControl.currentPage == count - 1) {
+        page = 0;
+    }else {
+        page = self.pageControl.currentPage + 1;
+    }
+    
+    CGFloat offSetX = page * width;
+    
+    [self.scrollView setContentOffset:CGPointMake(offSetX, 0)];
+    
+    
+    
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    CGFloat offSetX = scrollView.contentOffset.x;
+    CGFloat width = scrollView.bounds.size.width;
+    
+    NSUInteger page = (offSetX + width * 0.5) / width;
+    
+    self.pageControl.currentPage = page;
 }
 
 @end
